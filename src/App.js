cef65d2a4052701b891cc2aca260fb2fc1dda488
Paveltrs <84628503+Paveltrs@users.noreply.github.com>
2021-10-21 21:03:45 +0300

    added ability to store auths users in database

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from './pages/homepage/homepage'
 import ShopPage from './pages/shop/shop'
 import Header from './components/header/header'
 import SignPage from './pages/sign/sign'
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 
 import './App.css';
@@ -22,8 +22,20 @@ constructor() {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        })
+      }
+      this.setState({ currentUser: userAuth });
     })
   }
 
--- src/pages/sign/sign.jsx
+++ src/pages/sign/sign.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import SignIn from '../../components/sign-in/sign-in'
+import SignUp from '../../components/sign-up/sign-up'
 
 import './sign.scss'
 
 const SignPage = () => (
     <div className="sign">
         <SignIn />
+        <SignUp />
     </div>
 )
 
