52bb11e295231420ca4304e99f12023caea0cf51
Paveltrs <84628503+Paveltrs@users.noreply.github.com>
2021-10-21 21:10:10 +0300

    finished sign method

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {signInWithGoogle} from '../../firebase/firebase.utils'
+import { auth, signInWithGoogle} from '../../firebase/firebase.utils'
 
 import FormInput from '../form-input/form-input'
 import CustomButton from '../button/button'
@@ -16,10 +16,19 @@ class SignIn extends React.Component {
             password: ''
         }
     }
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
 
-        this.setState({ email: '', password: '' });
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: '', password: '' });
+        }
+        catch (error) {
+            console.log(error.message);
+        }
+        
     }
 
     handleChange = event => {
@@ -34,14 +43,17 @@ class SignIn extends React.Component {
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
                 <form onSubmit={this.handleSubmit}>
-                    <FormInput name="email" 
+                    <FormInput 
+                    name="email" 
                     type="email" 
                     value={this.state.email} 
                     handleChange={this.handleChange}
                     label="Email"
                     required />
 
-                    <FormInput name="password" type="password" 
+                    <FormInput 
+                    name="password" 
+                    type="password" 
                     value={this.state.password} 
                     handleChange={this.handleChange}
                     label="Password"
